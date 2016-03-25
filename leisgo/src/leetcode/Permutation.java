18da00bd88270a3f5bc172ef5ec6c9363dddc644
Lei Sun <sunray0620@gmail.com>
2016-03-25 00:52:49 -0700

    remove .idea folder

--- leisgo/src/leetcode/Permutation.java
+++ leisgo/src/leetcode/Permutation.java
@@ -14,20 +14,58 @@ public class Permutation {
      * @param nums: an array of distinct numbers.
      * @return: All permutations of input numbers.
      */
-    public List<List<Integer>> permuteWithSwap(int[] nums) {
-        return null;
+    public List<List<Integer>> getPermutationsWithSwap(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        // Corner cases:
+        if (nums == null || nums.length == 0) {
+            return results;
+        }
+
+        //
+        List<Integer> tempRet = new ArrayList<Integer>(nums.length);
+        boolean[] isUsed = new boolean[nums.length];
+        getPermutationWithSwap(nums, 0, results);
+        return results;
     }
 
-    public void getPermutationWithExtraSpace(int[] nums, int startPos, int[] result, List<List<Integer>> results) {
-        if (startPos == nums.length) {
-            results.add(new ArrayList(Arrays.asList(result)));
+    public void getPermutationWithExtraSpace(int[] numbers, int startPos, boolean[] isUsed, List<Integer> result, List<List<Integer>> results) {
+        if (startPos == numbers.length) {
+            results.add(new ArrayList(result));
             return;
         }
 
-        for (int i = startPos; i < nums.length; ++i) {
-            result[startPos] = nums[i];
-            getPermutationWithExtraSpace(nums, startPos + 1, result, results);
+        for (int i = 0; i < numbers.length; ++i) {
+            if (isUsed[i]) {
+                continue;
+            }
+            result.add(numbers[i]);
+            isUsed[i] = true;
+            getPermutationWithExtraSpace(numbers, startPos + 1, isUsed, result, results);
+            isUsed[i] = false;
+            result.remove(result.size() - 1);
         }
     }
 
+    public void getPermutationWithSwap(int[] numbers, int startPos, List<List<Integer>> results) {
+        if (startPos == numbers.length) {
+            List<Integer> tempResult = new ArrayList<>(numbers.length);
+            for (int v : numbers) {
+                tempResult.add(v);
+            }
+            results.add(tempResult);
+            return;
+        }
+
+        for (int i = startPos; i < numbers.length; ++i) {
+            swapElement(numbers, startPos, i);
+            getPermutationWithSwap(numbers, startPos + 1, results);
+            swapElement(numbers, startPos, i);
+        }
+    }
+
+    private void swapElement(int[] nums, int m, int n) {
+        int swapTemp = nums[m];
+        nums[m] = nums[n];
+        nums[n] = swapTemp;
+    }
 }
--- leisgo/src/leetcode/Playground.java
+++ leisgo/src/leetcode/Playground.java
@@ -1,18 +1,14 @@
 package leetcode;
 
+import java.util.List;
+
 /**
  * Created by sunlei on 3/12/16.
  */
 public class Playground {
     public static void main(String[] args) {
-        NextPermutation np = new NextPermutation();
-        int[] testInts = new int[] { 1, 1, 5 };
-        for (int i = 0; i < 20; ++i) {
-            printIntArray(testInts);
-
-            //
-            np.nextPermutation(testInts);
-        }
+        Permutation p = new Permutation();
+        List<List<Integer>> permutations = p.getPermutationsWithSwap(new int[] {1, 2, 3});
     }
 
     private static void printIntArray(int[] nums) {
